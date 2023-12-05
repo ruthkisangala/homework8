52c6949b44f981f814bab35ad66eb5f4cf6d02d6
Ruth Klocker <ruth.klocker@gmail.com>
2023-12-05 11:57:22 +0100

    Second task completed

--- homework8.py
+++ homework8.py
@@ -7,12 +7,24 @@ def task_one_dict():
     for i in range(1, n+1):
         value = i**2
         values.append(value)
-    combined_list = {keys[i]: values[i] for i in range(0, n)}
-    print(combined_list)
+    combined_dict = {keys[i]: values[i] for i in range(0, n)}
+    print(combined_dict)
+    return combined_dict
     
-task_one_dict()
-
+combined_dict = task_one_dict()
 
 # Task 2
+def adding(dict_input):
+   
+    sum_keys = 0
+    sum_values = 0
 
+    for key, value in dict_input.items():
+        sum_keys += int(key)
+        sum_values += int(value)
+    
+    print(sum_keys)
+    print(sum_values)
+    #return sum_keys, sum_values
 
+adding(combined_dict)
